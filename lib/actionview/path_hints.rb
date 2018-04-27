199845f1bed9a73e536f31a1ee6fc1089a4d13e2
ryan tulino <rtulino@weblinc.com>
2018-04-27 19:15:47 -0400

    support both rails 4 and 5

--- lib/actionview/path_hints.rb
+++ lib/actionview/path_hints.rb
@@ -1,13 +1,22 @@
 module ActionView
   class PathHints
     def self.apply(view, template, output_buffer)
-      cache_hit = view.view_renderer.cache_hits[template.virtual_path]
-      cache_color = cache_hit.nil? ? 'red' : 'green'
+      cache_color = cache_hit?(view, template) ? 'red' : 'green'
       container_style = "position: relative;border:1px solid #{cache_color}!important;"
       label_style = "font-size: 9px;background:beige;position:absolute;top:0px;left:0px;"
       path_hints = "<div style='#{container_style}'>"\
     "<span style='#{label_style}'>#{template.inspect}</span>"
       output_buffer.prepend(path_hints.html_safe).concat('</div>'.html_safe)
+    end
+
+    private
+
+    def cache_hit?(view, template)
+      rails_latest? ? view.view_renderer.cache : view.view_renderer.cache_hits[template.virtual_path]
+    end
+
+    def rails_latest?
+      Gem::Version.new(Rails.version) >= Gem::Version.new('5.0.0')
     end
   end
 
