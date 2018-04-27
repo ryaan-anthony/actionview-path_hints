26a3893ddd8abafe02e543ea079749f0695cddc1
ryan tulino <rtulino@weblinc.com>
2018-04-27 19:32:39 -0400

    had it backwards

--- lib/actionview/path_hints.rb
+++ lib/actionview/path_hints.rb
@@ -18,8 +18,8 @@ module ActionView
 
     def cache_hit?
       rails_latest? ?
-          @view.view_renderer.cache :
-          @view.view_renderer.cache_hits[@template.virtual_path]
+          @view.view_renderer.cache_hits[@template.virtual_path] :
+          @view.view_renderer.cache
     end
 
     def rails_latest?
